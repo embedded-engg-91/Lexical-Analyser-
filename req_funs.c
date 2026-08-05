f1704bdc1cc3caa27097f2690bf078d979149606
embedded-engg-91 <badigermanoj781@gmail.com>
2026-08-05 18:32:14 +0530

    Refactor token_type and related functions
    Removed unnecessary comments and cleaned up variable initialization.

--- req_funs.c
+++ req_funs.c
@@ -2,8 +2,8 @@
 
 Status token_type(char *token)
 {
-    //nedd to iterate theought the array of stgs and comapre and 
-    //see whther it matches or noot 
+    
+    
     int i=0;
     while(reserved[i]!=NULL)
     {
@@ -13,8 +13,8 @@ Status token_type(char *token)
         }
         i++;
     }
-    /* come out of this loop means not a reserved keyword*/
-    i=0; //reset for non reserved na 
+    
+    i=0; 
     while(non_reserved[i]!=NULL)
     {
         if(strcmp(non_reserved[i], token) == 0)
@@ -23,9 +23,9 @@ Status token_type(char *token)
         }
         i++;
     }
-    /*if come till here means it is not a non reseved keyowrd also */
     
-    /*if all the above fails means the token is deifnatrly ands identifier*/
+    
+    
     return IDEN;
 }
 Status is_operator(char token)
@@ -43,7 +43,7 @@ Status is_operator(char token)
 }
 Status is_brace(char token)
 {
-    int i=0; //reset for braces na 
+    int i=0; 
     while(braces[i]!=NULL)
     {
         if(braces[i][0] == token)
@@ -57,7 +57,7 @@ Status is_brace(char token)
 
 Status is_delim(char token)
 {
-    int i=0; //reset for delimiter na 
+    int i=0; 
     while(delimiter[i]!=NULL)
     {
         if(delimiter[i][0] == token)
@@ -72,7 +72,7 @@ Status is_delim(char token)
 
 Status is_directives(char *token)
 {
-    int i=0; //reset for delimiter na 
+    int i=0; 
     while(preprocessor_directives[i]!=NULL)
     {
         if(strcmp(preprocessor_directives[i], token) == 0)
